3c835161ee4d88ad7bd0715ec4cb2322cb50d2e1
fzbo <elizabeth.bazan@gmail.com>
2018-01-20 01:04:37 -0500

    First Update App In Progress

---
+++ questions.js
@@ -0,0 +1,19 @@
+//PSUEDO CODE-QUESTIONS FILE//
+//=================================//
+
+//DATA FOR QUESTIONS
+
+//var BasicCard = require('./BasiCard');
+//var data = [
+// {number: "Q1", questions: "Buildout questions to be asked," answer: "Answer"},
+// {number: "Q2", questions: "Buildout questions to be asked," answer: "Answer"},
+// {number: "Q3", questions: "Buildout questions to be asked," answer: "Answer"},
+// {number: "Q4", questions: "Buildout questions to be asked," answer: "Answer"},
+// {number: "Q5", questions: "Buildout questions to be asked," answer: "Answer"},
+// {number: "Q6", questions: "Buildout questions to be asked," answer: "Answer"},
+// {number: "Q7", questions: "Buildout questions to be asked," answer: "Answer"},
+// {number: "Q8", questions: "Buildout questions to be asked," answer: "Answer"},
+// {number: "Q9", questions: "Buildout questions to be asked," answer: "Answer"},
+// {number: "Q10", questions: "Buildout questions to be asked," answer: "Answer"},
+
+//];
